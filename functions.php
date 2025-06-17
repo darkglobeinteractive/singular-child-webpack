b310b6d0aa330392405abc6feca93595b2b10bfb
Robbie Schneider <darkglobeinteractive@gmail.com>
2025-06-17 10:58:39 -0400

    Webpack Conversion: Updating pathing to assets

--- functions.php
+++ functions.php
@@ -37,16 +37,14 @@ add_action( 'after_setup_theme', 'singchild_setup_theme_supported_features', 30 
 
 /* STYLES ------------------------------------------------------- */
 function singchild_enqueue_style() {
-
-	wp_enqueue_style( 'singchild-style', get_stylesheet_directory_uri().'/style.css', array(), singular_child_theme_filemtime( '/style.css' ) );
-  wp_enqueue_style( 'singchild-style-responsive', get_stylesheet_directory_uri().'/style-responsive.css', array(), singular_child_theme_filemtime( '/style-responsive.css' ) );
-
+	wp_enqueue_style( 'singchild-public-style', get_stylesheet_directory_uri().'/assets/public.css', array(), singular_child_theme_filemtime( '/assets/public.css' ) );
+  wp_enqueue_script( 'singchild-public-js', get_stylesheet_directory_uri().'/assets/public.min.js', array( 'jquery' ), singular_child_theme_filemtime( '/assets/public.min.js' ) );
 }
 add_action( 'wp_enqueue_scripts', 'singchild_enqueue_style', 99 );
 
 /* ADMIN STYLES ------------------------------------------------- */
 function singchild_admin_styles() {
-  wp_enqueue_style( 'admin-style-css', get_stylesheet_directory_uri().'/style.css', array(), singular_child_theme_filemtime('/style.css' ) );
+  wp_enqueue_style( 'singchild-admin-style', get_stylesheet_directory_uri().'/assets/admin.css', array(), singular_child_theme_filemtime('/assets/admin.css' ) );
 }
 add_action( 'admin_enqueue_scripts', 'singchild_admin_styles', 99 );
 
@@ -55,3 +53,4 @@ function singchild_login_css() {
   wp_enqueue_style( 'login-css', get_stylesheet_directory_uri().'/css/login.css', array(), singular_child_theme_filemtime('/css/login.css' ) );
 }
 add_action( 'login_enqueue_scripts', 'singchild_login_css' );
+?>
